c1c3ec110f122d9962e4aaa801c88045511ecf41
SherazJabbar <41358432+SherazJabbar@users.noreply.github.com>
2021-12-30 16:56:16 +0500

    Update firebaseInit.js

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -2,12 +2,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAq6lFfELnXiO4TjV9kAo5tBM42ke2XukE",
-  authDomain: "blogwebsite-78db8.firebaseapp.com",
-  projectId: "blogwebsite-78db8",
-  storageBucket: "blogwebsite-78db8.appspot.com",
-  messagingSenderId: "854367905483",
-  appId: "1:854367905483:web:733f62e9299d5b76605d4e",
+
 };
 
 // Initialize Firebase
